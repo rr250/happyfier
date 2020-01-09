63edb8d0c365ab8677200f553639f6a750ba0451
rr250 <rr250@snu.edu.in>
2020-01-10 00:04:53 +0530

    Added auth Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
-import {BrowserRouter,Switch,Route} from 'react-router-dom'
-import Navbar from './components/layout/Navbar'
-import Dashboard from './components/dashboard/Dashboard'
-import ProjectDetails from './components/project/ProjectDetails'
-
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Dashboard from "./components/dashboard/Dashboard";
+import ProjectDetails from "./components/project/ProjectDetails";
+import SignIn from "./components/auth/SignIn";
+import SignUp from './components/auth/SignUp'
 class App extends Component {
   render() {
     return (
@@ -11,8 +12,10 @@ class App extends Component {
         <div className="App">
           <Navbar />
           <Switch>
-            <Route exact path='/' component={Dashboard}/>
-            <Route path='/project/:id' component={ProjectDetails}/>
+            <Route exact path="/" component={Dashboard} />
+            <Route path="/project/:id" component={ProjectDetails} />
+            <Route path="/signin" component={SignIn} />
+            <Route path="/signup" component={SignUp} />
           </Switch>
         </div>
       </BrowserRouter>
