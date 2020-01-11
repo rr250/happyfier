e57dc7de8f8c2ae721e33899678481b4458d542e
rr250 <rr250@snu.edu.in>
2020-01-11 13:32:33 +0530

    Bug Fixes

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,6 @@ import ProjectDetails from "./components/project/ProjectDetails";
 import SignIn from "./components/auth/SignIn";
 import SignUp from './components/auth/SignUp';
 import CreateProject from './components/project/CreateProject'
-import NewTest from './components/test/NewTest'
-import Depression from "./components/test/Depression";
-import Anxiety from "./components/test/Anxiety";
 class App extends Component {
   render() {
     return (
@@ -21,9 +18,6 @@ class App extends Component {
             <Route path="/signin" component={SignIn} />
             <Route path="/signup" component={SignUp} />
             <Route path="/create" component={CreateProject} />
-            <Route exact path="/test" component={NewTest} />
-            <Route exact path="/test/depression" component={Depression} />
-            <Route exact path="/test/anxiety" component={Anxiety} />
           </Switch>
         </div>
       </BrowserRouter>
