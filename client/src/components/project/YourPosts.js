29b11d2eec47e747975b807bdbafb269e9a932b2
rr250 <rr250@snu.edu.in>
2020-01-12 15:16:35 +0530

    Bug Fixes

--- client/src/components/project/YourPosts.js
+++ client/src/components/project/YourPosts.js
@@ -4,6 +4,7 @@ import {firestoreConnect} from 'react-redux-firebase'
 import {compose} from 'redux'
 import {Redirect} from 'react-router-dom'
 import {deleteProject} from '../../store/actions/projectActions'
+import {Link} from 'react-router-dom'
 
 class YourPosts extends Component{
     handleDelete=(id,e)=>{
@@ -22,6 +23,7 @@ class YourPosts extends Component{
             <div className="dashboard container">
                 <div className="row">
                     <div className="col s12 m7">
+                    <h4>Personal Diary</h4>
                     {projects && projects.map(project=>{
                         if(auth.uid===project.authorId)return(
                             <div class="card">
@@ -39,14 +41,16 @@ class YourPosts extends Component{
                     })}                        
                     </div>
                     <div className="col s12 m4 offset-m1">
+                    <h5>Bookmarks</h5>
                     {projects && projects.map(project=>{
                         if(this.props.profile.bookMarks.includes(project.id))return(
+                            <Link to={'/project/' + project.id} key={project.id}>
                             <div class="card">
                                 <div class="card-content">
                                     <span class="card-title">{project.title}</span>
-                                    <p>{project.content}</p>
                                 </div>
                             </div>
+                            </Link>
                         )
                         else
                             return null
