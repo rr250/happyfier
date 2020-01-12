7f36f697ba1291dc0181283cd7c9c4120cd46780
Rishabh Ranjan <rr250@snu.edu.in>
2020-01-12 21:22:51 +0530

    Update fbConfig.js

--- client/src/config/fbConfig.js
+++ client/src/config/fbConfig.js
@@ -5,14 +5,7 @@ import "firebase/messaging"
 
 
 var config = {
-    apiKey: "AIzaSyCbfgRhgHE4sA1fxx1uYN77CN_GCQ5yH1I",
-    authDomain: "happyfier.firebaseapp.com",
-    databaseURL: "https://happyfier.firebaseio.com",
-    projectId: "happyfier",
-    storageBucket: "happyfier.appspot.com",
-    messagingSenderId: "64560973553",
-    appId: "1:64560973553:web:254bca329e65ae1a240b50",
-    measurementId: "G-25EMJ5FQJS"
+
   };
   const initializedFirebaseApp = firebase.initializeApp(config);
   firebase.firestore().settings({timestampsInSnapshots: true});
