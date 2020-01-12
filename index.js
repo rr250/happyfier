77d327d6399f9f9afc8d9c8f51c63d8c8b16f723
rr250 <rr250@snu.edu.in>
2020-01-12 21:16:47 +0530

    Bug Fixes

--- index.js
+++ index.js
@@ -3,20 +3,6 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const config = require('./config/keys');
-const mongoose = require('mongoose');
-mongoose.connect(config.mongoURI, { useNewUrlParser: true });
-
-require('./models/Registration');
-require('./models/Demand');
-require('./models/Coupons');
-
-
-app.use(bodyParser.json());
-
-require('./routes/dialogFlowRoutes')(app);
-require('./routes/fulfillmentRoutes')(app);
-
 if (process.env.NODE_ENV === 'production') {
     // js and css files
     app.use(express.static('client/build'));
